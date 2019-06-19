05abb059deb3fe51fe4ce443a8eb33dd562eb935
rifky <rifkyhilman265@gmail.com>
2019-06-19 17:16:34 +0700

    new project

---
+++ w2-Rabu2.js
@@ -0,0 +1,11 @@
+console.log('LOOPING PERTAMA')
+
+for(var deret = 1; deret <= 20; deret += 1) {
+  console.log(deret + ' - I love coding');
+}
+
+console.log('LOOPING KEDUA');
+
+for(var deret = 20; deret > 0; deret -= 1) {
+  console.log(deret +  ' - I will become fullstack developer');
+}
