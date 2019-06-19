20448c882ec06284e1d20b3d32ccc658d3193bd8
rifky <rifkyhilman265@gmail.com>
2019-06-19 22:15:34 +0700

    modified loop while

--- w2-Rabu2.js
+++ w2-Rabu2.js
@@ -1,11 +1,39 @@
 console.log('LOOPING PERTAMA')
 
-for(var deret = 1; deret <= 20; deret += 1) {
-  console.log(deret + ' - I love coding');
+var flag = 1;
+while(flag <= 20) {
+  console.log(flag + ' - I love codding');
+  flag++; 
 }
 
-console.log('LOOPING KEDUA');
+console.log('LOOPING KEDUA')
 
-for(var deret = 20; deret > 0; deret -= 1) {
-  console.log(deret +  ' - I will become fullstack developer');
+var flag = 20
+while(flag >= 1){
+  console.log(flag + ' - I will become fullstack developer')
+  flag--;
 }
+
+
+
+console.log('LOOPING PERTAMA')
+
+var row = 1;
+
+while(row <= 20) {
+  if(row % 2 == 0) {
+    console.log(row + ' - I love coding')
+  }
+  row = row + 1
+}
+
+console.log('LOOPING KEDUA')
+
+var row = 20;
+
+while(row >= 1) {
+  if (row % 2 == 0) {
+    console.log(row + ' - I will become fullstack developer')
+}
+ row--;
+} 
