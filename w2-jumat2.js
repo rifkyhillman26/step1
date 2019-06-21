91c8e05a9d3fc42d51132a3995ba4db92f2d5c62
rifky <rifkyhilman265@gmail.com>
2019-06-21 20:29:03 +0700

    new project

--- w2-jumat2.js
+++ w2-jumat2.js
@@ -1,7 +1,11 @@
-function balikKata(kata) {
-    // you can only write your code here!
-  return kata.split('').reverse().join('');
-  }
+ function balikKata(kata) {
+     // you can only write your code here!
+     var a='';
+     for(var i=kata.length -1; i >=0; i--){
+     a=a+kata[i]; 
+     }
+     return a;
+  } 
   
   // TEST CASES
   console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
